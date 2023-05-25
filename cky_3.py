91e8a747686c66c01f12a5ac0425d0d69aec435e
annazakreva <annazak102@gmail.com>
2023-05-25 17:01:16 +0200

    CNF en un fitxer apart

---
+++ cky_3.py
@@ -0,0 +1,230 @@
+from itertools import product
+import nltk
+import time
+
+GRAMMAR_FILE = 'grammarPLH.txt'
+WORD = 'hwshmiht'
+
+
+class Node:
+
+    def __init__(self, parent, child1,prob = None, child2 = None):
+        self.parent =  parent
+        self.probability = prob
+        self.child1 = child1
+        self.child2 = child2
+
+    def __repr__(self) -> str:
+        return f"Pointer parent {self.parent}"
+    
+class Dynamic_CKY:
+    
+    def __init__(self, grammar_file, timing = False, prob=False):
+        self.grammar = grammar_file
+        self.timing = timing
+        initial_time = time.time()
+        self.convert_grammar(prob) # We convert grammar file to grammar
+        if self.timing:
+            print("Time taken to convert grammar:", '{:.10f}'.format(time.time()-initial_time))
+        self.amount_rules = len(self.t_rules) # Save number of rules
+
+    def read_file(self):
+        '''
+        This function is used to read a '.txt' file containing the rules of a certain grammar.
+        '''
+        # Open the file in read mode using the file name
+        with open(self.grammar, 'r', encoding='utf-8') as file:
+            # Read the contents of the file and save them as a class variable
+            contents = file.read()
+            return contents
+
+    def convert_grammar(self, prob):
+        ''' 
+        Converts the grammar rules into the format the class works with.
+        Args:
+            self.contents('.txt' file): grammar rules 
+        Generates:
+            encoding_dict(dictionary): Encoding used for the rules, where the key is the head of the rule and the body is the numerical encoding used.
+            self.t_rules(list): Grammar rules, considering only the ones that derive terminals. 
+            self.nt_rules(list): Grammar rules, considering only the ones that derive non-terminals.
+        '''
+        grammar = self.read_file() #Reading the grammar file
+        # Split the grammar string into separate lines
+        grammar_lines = grammar.strip().split('\n')
+        print('input',grammar_lines)
+        #encoding the rules: each rule name will be encoded with a number from 0 to the amount of rules-1
+        self.encoding_dict, encoding = {}, 0 # Creating an empty dictionary to store the rules and initializing the encoding
+        for line in range(len(grammar_lines)):
+            self.encoding_dict[grammar_lines[line][0]] = encoding
+            self.encoding_dict[encoding] = grammar_lines[line][0]
+            encoding += 1
+        #Creating a list for each type of rule (terminal and non-terminal). The index states the head code and the content its body
+        self.t_rules, self.nt_rules = [[] for _ in range(encoding)], [[] for _ in range(encoding)]
+        if prob: #Creating the same mirrored lists for their corresponding probabilities
+            self.t_probs, self.non_t_probs =  [[] for _ in range(encoding)], [[] for _ in range(encoding)]
+        for line in grammar_lines: # Process each line of the grammar
+            head, elements = line.split(' → ') # Splitting the line into the head and elements of the rule
+            values = elements.split(' | ') # Splitting the elements using "|" as the separator, for the cases of 'OR' statements
+            for element in values: 
+                if element.isupper(): #it's a non-terminal
+                    #Creating a tuple so that 'AB' ~ ('A','B'), for future implementation use
+                    tuple_form = (self.encoding_dict[element[0]],self.encoding_dict[element[1]]) 
+                    self.nt_rules[self.encoding_dict[head]].append(tuple_form)
+                    if prob:
+                        #Appending the probability for each tuple in its corresponding place in the probability list
+                        self.non_t_probs[self.encoding_dict[head]].append(float(element[3:7]))
+                else: #is a terminal
+                    self.t_rules[self.encoding_dict[head]].append(element[0])
+                    #Appending the probability for the terminals
+                    if prob: 
+                        self.t_probs[self.encoding_dict[head]].append(float(element[2:6]))
+    
+   
+
+    def test_word(self, word, visual = True, prob=False):
+        ''' 
+        Tests a certain word for the processed grammar.
+        Input:
+            word(string): The word to check by the CKY.
+            visual: Boolean (True if output is visual parse tree)
+            prob: Boolean (True if taking into consideration probability is wanted)
+        Returns:
+            boolean: True or False according to belonging to the grammar.
+        Example:
+            >>> cky = Dynamic_CKY(grammar)
+            >>> cky.test_word('aabab')
+            True
+        '''
+        initial_time = time.time()
+        self.word = word
+        self.length = len(word)
+        if prob:
+            self.build_table_prob()
+        else:
+            self.build_table()
+        mid_time = time.time()
+        if self.timing:
+            print("Time taken to check word",'{:.10f}'.format(mid_time-initial_time))
+        if visual and self.table[0][0][self.encoding_dict['S']]: 
+            tree = self.visualize_tree()
+            if self.timing:
+                print("Time taken to draw tree",'{:.10f}'.format(time.time()-mid_time))
+            tree.draw()
+            print(self.pointers)
+        
+        
+        if prob:
+            print(self.probabilities)
+
+        return self.table[0][0][self.encoding_dict['S']]
+
+    def build_table(self):
+        '''
+        Function used to test current word. It creates 2 tables:
+        - table: Built as a table of nxn, where each element is a boolean list of amount_rules length. 
+            If cell is True in the i position, then the ith rule can be applied there
+        - pointers: Built as the previous table, it saves the nodes that connect the rules to visualize Parse Tree
+        '''
+        # Creation of tables
+        self.table = [[[False for _ in range(self.amount_rules)] for p in range(self.length)]for i in range(self.length)]
+        self.pointers = [[[None for _ in range(self.amount_rules)] for p in range(self.length)]for i in range(self.length)] 
+
+
+        # Iteration of last row (for terminals)
+        for terminal in range(self.length):
+            for i_rule, rule in enumerate(self.t_rules):
+                # Turn the i_rule index of this cell True if the letter is found the body of the i_rule rule
+                self.table[self.length-1][terminal][i_rule] = True if self.word[terminal] in rule else False 
+                # We create a pointer with the parent being the index of the rule, and the (only) child the letter
+                self.pointers[self.length-1][terminal][i_rule] = Node(parent = i_rule, child1 = self.word[terminal])
+    
+        for i in range(self.length-2,-1, -1): # Iterating through rows
+            for j in range(0,i+1,1): # Iterating through columns
+                for i_rule, rule in enumerate(self.nt_rules): # Iterating through rules
+                    for body in rule: # Iterating through different bodies in rules
+                        # First possible cell combination
+                        i1, j1 = self.length-1, j 
+                        i2, j2 = i+1, j+1
+                        while i1 > i and not self.table[i][j][i_rule]: # While we haven't tried all cell combinations and rule is still not True
+                            if self.table[i1][j1][body[0]] and self.table[i2][j2][body[1]]: # If the body is True in the current combination 
+                                self.table[i][j][i_rule] = True 
+                                # Create pointer: Parent is rule index, and childs are the pointers of the cells we have used to apply the rule
+                                self.pointers[i][j][i_rule] = Node(parent = i_rule, child1 = self.pointers[i1][j1][body[0]],child2=self.pointers[i2][j2][body[1]])
+
+                            i1, i2, j2 = i1-1, i2+1, j2+1 
+
+    def build_table_prob(self):
+        '''
+        Function used to test current word with the probability extension added. It creates 3 tables:
+        - table: Built as a table of nxn, where each element is a boolean list of amount_rules length. 
+            If cell is True in the i position, then the ith rule can be applied there.
+        - pointers: Built as the previous table, it saves the nodes that connect the rules to visualize Parse Tree.
+        - probabilities: Built as the previous table but with only one contianer per cell, it keeps the maximum probability achieved per cell if there's a feasible combination.
+        '''
+        # Creation of tables
+        self.table = [[[False for _ in range(self.amount_rules)] for _ in range(self.length)]for _ in range(self.length)]
+        self.pointers = [[[None for _ in range(self.amount_rules)] for _ in range(self.length)]for _ in range(self.length)]
+        self.probabilities = [[0.0 for _ in range(self.length)] for _ in range(self.length)] 
+        # Iteration of last row (for terminals)
+        for terminal in range(self.length):
+            max_prob, best_rule = 0.0, -1  # Initialize maximum probability and best rule index
+            for i_rule, rule in enumerate(self.t_rules):
+                for ind, p in enumerate(self.t_probs[i_rule]):
+                    #Calculating highest probability per terminal
+                    if self.t_rules[i_rule][ind] == self.word[terminal] and p > max_prob:
+                        max_prob = p
+                        best_rule = i_rule
+            self.probabilities[self.length-1][terminal] = max_prob
+            # Turn the i_rule index of this cell True if the letter is found the body of the i_rule rule and has maximum probability
+            self.table[self.length-1][terminal][best_rule]= True
+            # We create a pointer with the parent being the index of the rule, and the (only) child the letter
+            self.pointers[self.length-1][terminal][best_rule] = Node(parent = best_rule, prob = max_prob, child1 = self.word[terminal])
+        for i in range(self.length-2,-1, -1): # Iterating through rows
+            for j in range(0,i+1,1): # Iterating through columns
+                max_prob = 0.0  # Initialize maximum probability
+                best_rule = -1  # Initialize best rule index
+                for i_rule, rule in enumerate(self.nt_rules): # Iterating through rules
+                    for i_body, body in enumerate(rule): # Iterating through different bodies in rules
+                        # First possible cell combination
+                        i1, j1 = self.length-1, j 
+                        i2, j2 = i+1, j+1
+                        while i1 > i: # While we haven't tried all cell combinations 
+                            if self.table[i1][j1][body[0]] and self.table[i2][j2][body[1]]: # If the body is True in the current combination 
+                                #Calculate the probability of the combination
+                                prob = self.non_t_probs[i_rule][i_body] * self.probabilities[i1][j1] * self.probabilities[i2][j2]
+                                #Checking if the probability is higher to what has been previously saved, if so save it
+                                if prob > max_prob:
+                                    max_prob = prob
+                                    # Create pointer: Parent is rule index, and childs are the pointers of the cells we have used to apply the rule
+                                    self.pointers[i][j][best_rule] = None
+                                    self.pointers[i][j][i_rule] = Node(parent = i_rule, prob = prob, child1 = self.pointers[i1][j1][body[0]],child2=self.pointers[i2][j2][body[1]])
+                                    best_rule = i_rule
+                            i1, i2, j2 = i1-1, i2+1, j2+1 
+                if best_rule > -1:#if there's a feasible combination 
+                    self.probabilities[i][j] = max_prob  # Set the maximum probability for the current cell
+                    # Only turning true the best rule
+                    self.table[i][j][best_rule]= True
+        print(self.table)
+    
+    def visualize_tree(self):
+        '''
+        Builds tree then draws it
+        '''
+        return self.build_tree(node=self.pointers[0][0][self.encoding_dict['S']])
+
+    def build_tree(self, node):
+        '''
+        Starting off at parent node, it returns the tree by building it recursively
+        '''
+        print(node)
+        if node.child2 is None: 
+            return nltk.Tree(self.encoding_dict[node.parent]+f'({node.probability})', [node.child1])
+        return nltk.Tree(self.encoding_dict[node.parent]+f'{node.probability}', [self.build_tree(node.child1), self.build_tree(node.child2)])
+
+
+
+if __name__ == '__main__':
+    cky = Dynamic_CKY(GRAMMAR_FILE, prob=True)
+    print(cky.test_word(WORD,prob=True))
+    
+    
---
+++ read_input.py
@@ -0,0 +1,190 @@
+import string
+
+grammar = 'grammar1.txt'
+with open(grammar, 'r') as file:
+    # Read the contents of the file and save them as a class variable
+    contents = file.read()
+
+def replace_letter(bodies, letter_to_combine):
+    result = []
+    for body in bodies:
+        body = [letter for letter in body]
+        if all([letter!=letter_to_combine for letter in body]):
+            permutations = [''.join(body)]
+        elif body != [letter_to_combine,letter_to_combine]:           
+            permutations = []
+            for combination in [letter_to_combine,None]:
+                new_values = []
+                for value in body:
+                    if value == letter_to_combine: # If value is A (needs to be replaced)
+                        if combination != None: # If it is not empty string, subtitute A with body
+                            new_values.append(combination)   
+                    else: # If it is not A, add value
+                        new_values.append(value)
+                permutations.append(''.join(new_values))
+            else:
+                if combination != None:
+                    permutations.append(''.join(body))
+        else:
+            permutations = [letter_to_combine+letter_to_combine, letter_to_combine]
+        result += permutations
+    return result
+
+def convert_grammar(grammar, cnf = True):
+    
+    # Split the grammar string into separate lines
+    grammar_lines = grammar.strip().split('\n')
+    heads, bodies = [], []
+    for rule_idx in range(len(grammar_lines)): # Process each line of the grammar
+        line = grammar_lines[rule_idx]
+        head, elements = line.split(' → ') # Splitting the line into the head and elements of the rule
+        values = elements.split(' | ') # Splitting the elements using "|" as the separator, for the cases of 'OR' statements
+        heads.append(head)
+        bodies.append(values)
+
+    print(heads)
+    print(bodies)
+    
+    if cnf:
+        alphabet = [letter for letter in string.ascii_uppercase if letter not in heads]
+        i = 0
+        while i < len(bodies):
+            for element in range(len(bodies[i])):
+                current_var = bodies[i][element]
+                #step 1:
+                ''' 
+                Eliminate terminals from right side if they exist with other terminals or non-terminals
+                e.g: production rule X->xY can be decomposed as:
+                X->ZY
+                Z->x
+                '''
+                if any(char.islower() for char in current_var) and len(current_var) > 1:
+                    new_var = ''
+                    for letter in range(len(current_var)):
+                        if current_var[letter].islower():
+                            new_letter = alphabet.pop(0)
+                            heads.append(new_letter)
+                            bodies.append([current_var[letter]])
+                            new_var += new_letter
+                        else:
+                            new_var += current_var[letter]
+                    bodies[i][element] = new_var
+                
+                current_var = bodies[i][element] #in case something has changed
+                
+                #step 2:
+                '''
+                Eliminate RHS with more than two non-terminals.
+                e.g,; production rule X->LYZ can be decomposed as:
+                X->LP
+                P->YZ
+                '''
+                if len(current_var) > 2:
+                    new_letter = alphabet.pop(0)
+                    new_var = current_var[0] + new_letter #Change to two terminals
+                    bodies[i][element] = new_var #change the body of the rule to a two terminals rule
+                    heads.append(new_letter) #add new created rule to heads
+                    bodies.append([current_var[1:]]) #add new rule (the rest of the body of the original one) to bodies 
+                
+                current_var = bodies[i][element] #in case something has changed    
+            i+=1
+
+
+        #step 3:
+        '''
+        Eliminate ε-rules
+        e.g: a production rule can derive is nullable in two scenarios:
+            - ε appears explicitly: A → ε or  A → a | ε 
+            - the rule is implicitly nullable: A → B where B doesn't exist
+        For purely nullable rules: all istances of the head are deleted
+        For partially nullable rules: consider that all appearances of the rule as possible empty string 
+        '''
+        #Dealing with implicitly nullable rules A → B
+        for body in range(len(bodies)):
+            current_body = bodies[body]
+            if 'ε' in current_body or ((len(current_body) == 1) and (current_body[0].isupper() and (len(current_body[0])==1)) ): # Rule is nullable
+                if len(current_body) == 1 and current_body != 'ε': 
+                    current_body == 'ε'
+                    bodies[body][element] = 'ε' #we delete A → B
+        
+        #dealing with explicitly pure nullable rule A → ε
+        for body in range(len(bodies)): 
+            current_body = bodies[body]
+            if 'ε' in current_body and len(current_body)==1:
+                to_delete = heads[body] #Rule that should be deleted from other rules
+                for rule in range(len(bodies)):
+                    if bodies[rule]:
+                        updated_rule = [element.replace(to_delete, '') for element in bodies[rule]] #delete all the heads appearences in each rule
+                        bodies[rule] = updated_rule
+                heads[body] = False
+                bodies[body] = False
+        
+        #updating lists to delete the null rules
+        bodies = [element for element in bodies if element != False]
+        heads = [element for element in heads if element != False]
+
+        #dealing with explicitly impure nullable rule A → a | ε 
+        for body in range(len(bodies)):
+            current_body = bodies[body]
+            if len(current_body) > 1 and 'ε' in current_body: 
+                current_body.remove('ε') #elements for the permutation
+                bodies[body] = current_body #we update the rule so it doesn't have ε 
+                #modify the production rules:
+                null_head = heads[body]
+                nulled_bodies = [] #list of bodies that contain the nullable rule
+                for body2 in range(len(bodies)): #we find the heads of the elements that contain the nullable rule
+                    for element in range(len(bodies[body2])):
+                        if null_head in bodies[body2][element]: #if the nullable rule appears in some other rule
+                            nulled_bodies.append(heads[body2]) 
+                for nulled in nulled_bodies: #3.1 i 3.2
+                    nulled_index = heads.index(nulled)
+                    new_bodies = replace_letter(bodies[nulled_index], null_head)
+                    bodies[nulled_index] = new_bodies
+                    changed_rule = heads[nulled_index]
+                    for body3 in range(len(bodies)):
+                        if any(changed_rule in el for el in bodies[body3]):
+                            new_bodies = replace_letter(bodies[body3], changed_rule)
+                            bodies[body3] = new_bodies
+
+        #step 4:
+        '''
+        Eliminate unit rules
+        e.g: considering production rule A → B:
+            if B → CD exists, original rule should be converted to  A → CD
+        '''
+        for body in range(len(bodies)):
+            current_body = bodies[body]
+            if ((len(current_body) == 1) and (current_body[0].isupper() and (len(current_body[0])==1)) ): # Rule is unitary
+                if heads.index(bodies[body]):
+                    actual_rule = bodies[heads.index(bodies[body])] #he look for B → CD, specifically CD
+                    bodies[body] = actual_rule
+        
+        #step 5:
+        '''
+        Filter unitary rules that are left
+        '''
+        bodies = [[item for item in sublist if not (len(item) == 1 and item.isupper())] for sublist in bodies]
+
+
+                       
+             
+    #encoding the rules: each rule name will be encoded with a number from 0 to the amount of rules-1
+    encoding_dict, encoding = {}, 0 # Creating an empty dictionary to store the rules and initializing the encoding
+    for line in range(len(heads)):
+        encoding_dict[heads[line]] = encoding
+        encoding += 1
+            
+    #Creating a list for each type of rule (terminal and non-terminal). The index states the head code and the content its body
+    ter_rules, nter_rules = [[] for _ in range(encoding)], [[] for _ in range(encoding)]
+    for head,body in zip(heads, bodies): # Process each line of the grammar
+        for element in body: 
+            if element.isupper(): #it's a non-terminal
+                #Creating a tuple so that 'AB' ~ ('A','B'), for future implementation use
+                tuple_form = (encoding_dict[element[0]],encoding_dict[element[1]]) 
+                nter_rules[encoding_dict[head]].append(tuple_form)
+            else: #is a terminal
+                ter_rules[encoding_dict[head]].append(element)
+
+    return ter_rules, nter_rules
+
+t_rules, nt_rules = convert_grammar(contents)
